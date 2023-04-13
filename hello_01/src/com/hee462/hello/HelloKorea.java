2c2d8970f1d4d668e0ba077717360bbd20ef42af
hee462 <a01080809887@gmail.com>
2023-04-13 10:19:59 +0900

    새로운_리파지토리

---
+++ hello_01/src/com/hee462/hello/HelloKorea.java
@@ -0,0 +1,11 @@
+package com.hee462.hello;
+
+public class HelloKorea {
+	public static void main(String[] args) {
+		System.out.println(1+2);
+		System.out.println(1-2);
+		System.out.println(1*2);
+		System.out.println(1/2);
+	}
+
+}
---
+++ hello_01/src/com/hee462/hello/HelloYour.java
@@ -0,0 +1,15 @@
+package com.hee462.hello;
+/*
+ *  Source Code, java Class File
+ */
+public class HelloYour {
+	
+	public static void main(String[] args) {
+		System.out.println(30+40);
+		System.out.println(30-40);
+		System.out.println(30*40);
+		System.out.println(30/40);
+		System.out.println(30%40);
+	}
+	
+}
